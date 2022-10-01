e114f3d0f7793c0d01b313128e103afdff13180c
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2022-10-01 20:22:21 -0300

    Criação telas Cadastro e arquivo da Home

---
+++ rotinas_app/lib/Cadastro.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'Home.dart';
+
+class Cadastro extends StatelessWidget{
+
+  dynamic username;
+  dynamic dataNascimento;
+  dynamic email;
+  dynamic senha;
+  dynamic senhaConfirm;
+
+  @override
+  Widget build(BuildContext context){
+    return _body(context);
+
+  }
+
+  _body(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("RotinasApp"),
+      ),
+      body: Container(
+        color: Colors.white60,
+        width: 200,
+        margin: EdgeInsetsDirectional.all(90),
+        child: Column(
+          children: [
+            Text("Cadastro", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black87),),
+            _textField('Username', false, username),
+            _textField('Data de Nascimento', false, dataNascimento),
+            _textField('Email', false, email),
+            _textField('Senha', true, senha),
+            _textField('Confirmar Senha', true, senhaConfirm),
+            _button("Entrar", context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  _textField(label, esconder, variavel){
+    if(esconder){
+      return Center(
+        child: TextFormField(
+          onChanged: (dynamic input) {
+            variavel = input;
+          },
+          decoration: InputDecoration(
+            labelText: label,
+            border: OutlineInputBorder(),
+            suffixIcon: Icon(
+              Icons.visibility,
+              color: Colors.black26,
+            ),
+            labelStyle: TextStyle(
+              color: Colors.teal,
+            ),
+          ),
+        ),
+      );
+    }else{
+      return Center(
+        child: TextFormField(
+          onChanged: (dynamic input) {
+            variavel = input;
+          },
+          decoration: InputDecoration(
+            labelText: label,
+            border: OutlineInputBorder(),
+            labelStyle: TextStyle(
+              color: Colors.teal,
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  _button(label, context){
+    return Center(
+      child: FloatingActionButton.extended(
+        backgroundColor: Colors.black54,
+        label: Text(label),
+        onPressed: (){
+          Navigator.pop(context, Home);
+        },
+      ),
+    );
+  }
+}
