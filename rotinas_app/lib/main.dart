6f52f861707047676ee5f14e7cc9abb0a228e57e
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2022-10-01 20:21:19 -0300

    Criação da tela de Cadastro e criação de rota para a tela de Home

--- rotinas_app/lib/main.dart
+++ rotinas_app/lib/main.dart
@@ -1,83 +1,19 @@
 import 'package:flutter/material.dart';
+import 'Cadastro.dart';
 
 main(){
-  runApp(AppWidget());
+  runApp(Principal());
 }
 
-class AppWidget extends StatelessWidget{
+class Principal extends StatelessWidget{
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: Login(),
+      home: Cadastro(),
     );
   }
-}
-
-class Login extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return _body();
-
-  }
-
-  _body(){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("RotinasApp"),
-      ),
-      body: Container(
-        color: Colors.white60,
-        width: 200,
-        margin: EdgeInsetsDirectional.all(90),
-        child: _textField('Email', false),
-      ),
-    );
-  }
-
-  _textField(label, esconder){
-    if(esconder){
-      return Center(
-        child: TextFormField(
-          decoration: InputDecoration(
-            labelText: label,
-            border: OutlineInputBorder(),
-            suffixIcon: Icon(
-              Icons.visibility,
-              color: Colors.black26,
-            ),
-            labelStyle: TextStyle(
-              color: Colors.teal,
-            ),
-          ),
-        ),
-      );
-    }else{
-      return Center(
-        child: TextFormField(
-          decoration: InputDecoration(
-            labelText: label,
-            border: OutlineInputBorder(),
-            labelStyle: TextStyle(
-              color: Colors.teal,
-            ),
-          ),
-        ),
-      );
-    }
-  }
-
-  _button(){
-    return Center(
-      child: FloatingActionButton.extended(
-        backgroundColor: Colors.black54,
-        label: Text("Entrar"),
-        onPressed: onPressed(),
-      ),
-    );
-  }
-
-  onPressed(){}
 }
