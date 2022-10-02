cf2e693296ddbfd94750669a4ef37a8a2b7109d7
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2022-10-02 10:22:08 -0300

    Body e AppBar Detalhes , CadastroRotina

--- rotinas_app/lib/CadastroRotina.dart
+++ rotinas_app/lib/CadastroRotina.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
+import 'Perfil.dart';
+import 'Home.dart';
 
 class CadastroRotina extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        actions: [
+          FloatingActionButton.extended(
+            onPressed: abrirHome(context),
+            label: Text('Home'),
+            backgroundColor: Colors.black26,
+          ),
+          FloatingActionButton(
+            onPressed: abrirPerfil(context),
+            mini: true,
+            foregroundColor: Colors.black26,
+            child: Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: _body(),
+    );;
   }
+
+  abrirHome(context){
+    Navigator.pop(context, Home);
+  }
+
+  abrirPerfil(context){
+    Navigator.pop(context, Perfil);
+  }
+
+  _body(){}
 }
