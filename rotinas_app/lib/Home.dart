b10dbfc72fe85501e44269540578b2454317d156
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2022-10-02 10:13:18 -0300

    Body e AppBar HomePage

--- rotinas_app/lib/Home.dart
+++ rotinas_app/lib/Home.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'Perfil.dart';
 
 class Home extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RotinasApp'),
+        backgroundColor: Colors.teal,
+        actions: [
+          FloatingActionButton(
+            onPressed: abrirPerfil(context),
+            mini: true,
+            foregroundColor: Colors.teal,
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      body: _body(),
+    );
+  }
+
+  abrirPerfil(context){
+    Navigator.pop(context, Perfil);
+  }
+
+  _body(){
+    
   }
 }
