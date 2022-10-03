5fca99e3cbfac78e336b94ec170a3cfb05a2643b
Victor Leite De Andrade <1206056@sga.pucminas.br>
2022-10-03 10:12:55 -0300

    Shared Preference com erro

--- rotinas_app/lib/Cadastro.dart
+++ rotinas_app/lib/Cadastro.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'Login.dart';
 import 'Home.dart';
 
-class Cadastro extends StatelessWidget {
-  const Cadastro({super.key});
+class Cadastro extends StatefulWidget {
+  @override
+  State<Cadastro> createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  TextEditingController userName = TextEditingController();
+  TextEditingController dataNascimento = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController senha = TextEditingController();
+  TextEditingController confirmSenha = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +30,11 @@ class Cadastro extends StatelessWidget {
                 Text("Cadastro", style: TextStyle(fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: Colors.teal),),
-                _textField('Username', false),
-                _textField('Data de Nascimento', false),
-                _textField('Email', false),
-                _textField('Senha', true),
-                _textField('Confirmar Senha', true),
+                _textField('Username', false, userName),
+                _textField('Data de Nascimento', false, dataNascimento),
+                _textField('Email', false, email),
+                _textField('Senha', true, senha),
+                _textField('Confirmar Senha', true, confirmSenha),
                 _button("Entrar", context),
               ],
             )
@@ -32,10 +43,11 @@ class Cadastro extends StatelessWidget {
     );
   }
 
-  _textField(label, esconder) {
+  _textField(label, esconder, variavel) {
     if (esconder) {
       return Center(
         child: TextFormField(
+          controller: variavel,
           decoration: InputDecoration(
             labelText: label,
             border: OutlineInputBorder(),
@@ -52,6 +64,7 @@ class Cadastro extends StatelessWidget {
     } else {
       return Center(
         child: TextFormField(
+          controller: variavel,
           decoration: InputDecoration(
             labelText: label,
             border: OutlineInputBorder(),
@@ -69,10 +82,21 @@ class Cadastro extends StatelessWidget {
       child: FloatingActionButton.extended(
         backgroundColor: Colors.black54,
         label: Text(label),
-        onPressed: () =>
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => Home())),
-      ),
+        onPressed: salvar_trocarTela(context),
+      )
+    );
+  }
+
+  salvar_trocarTela(context) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user', userName.value.toString());
+    await prefs.setString('dataNascimento', dataNascimento.value.toString());
+    await prefs.setString('email', email.value.toString());
+    await prefs.setString('senha', senha.value.toString());
+    await prefs.setString('confirmSenha', confirmSenha.value.toString());
+
+    return Navigator.pushReplacement(context,
+      MaterialPageRoute(builder: (context) => Home())
     );
   }
 }
