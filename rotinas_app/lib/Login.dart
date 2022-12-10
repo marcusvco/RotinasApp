3b9db98160f565f7bd0628f25e762fcad87ec5a4
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2022-12-10 16:53:59 -0300

    Tela Login e Direcionamento para a tela de Login pela main

--- rotinas_app/lib/Login.dart
+++ rotinas_app/lib/Login.dart
@@ -1,9 +1,86 @@
 import 'package:flutter/material.dart';
+import 'Home.dart';
 
 class Login extends StatelessWidget{
+  TextEditingController userName = TextEditingController();
+  TextEditingController senha = TextEditingController();
+  dynamic _context;
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    this._context = context;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RotinasApp'),
+        backgroundColor: Colors.teal,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(80),
+        child: Center(
+            child: Column(
+              children: [
+                Text("Login", style: TextStyle(fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal),),
+                _textField('Username', false, userName),
+                _textField('Senha', true, senha),
+                _button("Entrar"),
+              ],
+            )
+        ),
+      ),
+    );
+  }
+
+  _textField(label, esconder, variavel) {
+    if (esconder) {
+      return Center(
+        child: TextFormField(
+          controller: variavel,
+          decoration: InputDecoration(
+            labelText: label,
+            border: OutlineInputBorder(),
+            suffixIcon: Icon(
+              Icons.visibility,
+              color: Colors.black26,
+            ),
+            labelStyle: TextStyle(
+              color: Colors.teal,
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: TextFormField(
+          controller: variavel,
+          decoration: InputDecoration(
+            labelText: label,
+            border: OutlineInputBorder(),
+            labelStyle: TextStyle(
+              color: Colors.teal,
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  _button(label) {
+    return Center(
+      child: FloatingActionButton.extended(
+        backgroundColor: Colors.black54,
+        label: Text(label),
+        onPressed: _entrar,
+      ),
+    );
+  }
+
+  _entrar(){
+    // Verificação se dados estão ok
+    //
+    return Navigator.pushReplacement(
+        this._context, MaterialPageRoute(builder: (context) => Home()));
   }
 }
--- rotinas_app/lib/main.dart
+++ rotinas_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Cadastro.dart';
 import 'Login.dart';
-import 'Home.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -32,7 +31,7 @@ class Principal extends StatelessWidget {
                   FloatingActionButton.extended(
                     backgroundColor: Colors.teal.shade400,
                     label: Text('Login'),
-                    onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home())),
+                    onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login())),
                   ),
                   FloatingActionButton.extended(
                     backgroundColor: Colors.teal.shade400,
